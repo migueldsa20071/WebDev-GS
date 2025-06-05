45c845b3b3acb71085a99e78dbdfb1ef197b0256
Miguel <migueldsa2007@gmail.com>
2025-06-05 14:20:30 -0300

    garantir que o Hamburguer ira funcionar através do JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 /* Espera o carregamento completo do DOM */
 document.addEventListener("DOMContentLoaded", () => {
 
+  // Menu Hambúrguer
+const hamburger = document.getElementById("hamburger");
+const navList = document.getElementById("navList");
+
+hamburger.addEventListener("click", function () {
+  navList.classList.toggle("open");
+});
+
+
 
 });
